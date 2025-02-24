6c246ecb4954f086994073f1bc95536ff5af8eda
YugoHasegawa <15hasegawa@gmai.com>
2025-02-24 12:31:46 +0900

    不要なフッターを削除

--- proud-plasma/src/components/sections/Footer.tsx
+++ proud-plasma/src/components/sections/Footer.tsx
@@ -4,15 +4,6 @@ import { appData } from "../../data/appData";
 const Footer: React.FC = () => {
 	return (
 		<footer className="py-8 text-center space-y-4 border-t border-white/5">
-			<div className="flex justify-center space-x-6 text-sm text-gray-400">
-				<a href="/privacy" className="transition-colors hover:text-white">
-					Privacy Policy
-				</a>
-				<a href="/terms" className="transition-colors hover:text-white">
-					Terms of Service
-				</a>
-			</div>
-
 			<div className="text-sm text-gray-500">
 				<p>
 					&copy; {new Date().getFullYear()}{" "}
